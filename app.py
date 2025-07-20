99776e1945774513204a375c4b40432f9212ef55
Akshay20051027 <aakshayguptha@gmail.com>
2025-07-20 18:40:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from flask_cors import CORS
+import os
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -83,4 +86,5 @@ def send_contact():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render-assigned port
+    app.run(debug=False, host='0.0.0.0', port=port)  # Allow external access
